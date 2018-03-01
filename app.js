96a83fb9974b2a57fed87fd2e26104f06f1f8be7
Anzumana Taal <anzumana.taal@me.com>
2018-03-01 09:42:20 +0100

    feat: switch to testfile and config folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var nb = require("vim-netbeans");
 var server = new nb.VimServer();
 var currentFile ='';
 server.on("clientAuthed", function (vim) {
-	console.log('cliein authenticated');
+	console.log('client authenticated');
 	vim.on("insert", function (buffer, offset, text) { console.log('sdsd');
 			console.log("Inserted text at " + offset + ": " + text);
 	});
@@ -11,7 +11,7 @@ server.on("clientAuthed", function (vim) {
 	});
 	vim.on("newBuffer", function (buffer) {
 		buffer.startDocumentListen();
-		console.log(buffer);
+		//console.log(buffer);
 	});
 	vim.on("fileOpened", function(pathname){
 		console.log('this is the file open handler');
@@ -20,18 +20,45 @@ server.on("clientAuthed", function (vim) {
 		currentFile = getFilename(pathname);
 		console.log(currentFile);
 	});
-	vim.key("C-i", function (buffer, offset, lnum, col) {
+	vim.key("C-o", function (buffer, offset, lnum, col) {
 		// insert a timestamp before the current line
 		console.log("edit buffer");
-		buffer.editFile('/Users/anzumana/Documents/git/mobi_template/__tests__/' +currentFile);
+		// get location of testfile from package json
+		buffer.editFile('/Users/anzumana/Documents/git/jvd/__tests__/' +currentFile);
 		//var num = new Date().toString();
 		//buffer.insert(offset - col, num);
 	});
+	/**
+		* Switch between implementation and itest file 
+		*
+	*/
+
+	vim.key("C-t", function (buffer, offset, lnum, col) {
+		console.log("edit buffer");
+		console.log(buffer.pathname);
+		if(buffer.pathname.indexof('__tests__/')!== -1){
+			//go to test file
+		} else{
+			//go to implementation file
+		}
+		//buffer.setFullName('/Users/anzumana/Documents/git/jvd/__tests__/' +currentFile);
+	});
 	function getFilename(pathname){
-		console.log(pathname);
-		var array = pathname.split('/');
-		var name = array[array.length-1];
-		return name 
+		console.log('getFilename');
+		console.log(pathname + 'asdasd');
+
+		if(typeof(pathname) ==="string"){
+			console.log(pathname);
+			var array = pathname.split('/');
+			var name = array[array.length-1];
+			return name 
+		} if(pathname.pathname !== undefined){
+				var array = pathname.pathname.split('/');
+				var name = array[array.length-1];
+				return name 
+		}else{
+			return 'dummy'
+		}
 		
 	}
 	
